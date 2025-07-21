7a4486abf5c1cb3da21dece9f8bdd2d7b355f22e
Yara Samy Fouad Hafez <ysfh_ben_18@edu.helwan.edu.eg>
2025-07-22 01:50:07 +0300

    Refactor: Consolidate stylesheets and update imports

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
+import "./styles/index.css";
+import "./styles/signup.css";
+import "./styles/details.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Error404 from "./pages/Error404";
 import Layout from "./pages/Layout";
+
 import { menuData } from "./data";
+import Products from "./pages/Products";
+// import ProductsCart from "./componants/productsCart";
+
+console.log("Menu Data:", menuData);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
