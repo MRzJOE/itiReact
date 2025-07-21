516f28c973ec35c412b7b88dfade8cf454c359a2
Yara Samy Fouad Hafez <ysfh_ben_18@edu.helwan.edu.eg>
2025-07-22 01:50:07 +0300

    Feature: Implement dynamic product display

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { menuData } from "../data";
 
 function Products() {
-  return <h1>products</h1>;
+  const menuDate = menuData;
+  const handleAddToCart = () => {
+    // console.log(`${title} added to cart!`);
+  };
+
+  return (
+    <section id="Products" className="container-sm my-5">
+      <div className="mb-3">
+        <p className="display-5">Our Products</p>
+      </div>
+      <div className="row">
+        {menuDate.pizza.map((pizzaa) => (
+          <div key={pizzaa.sku} className="col-md-3 mb-3">
+            <div className="card h-100">
+              <img
+                src={pizzaa.image}
+                className="card-img-top"
+                alt={pizzaa.name}
+              />
+              <div className="card-body d-flex flex-column">
+                <h6 className="text-muted">{pizzaa.category}</h6>
+                <h5 className="card-title">{pizzaa.name}</h5>
+                <p className="card-text mt-4">${pizzaa.price.toFixed(2)}</p>
+                <button
+                  className="card-text mt-auto btn-primary rounded-2 py-2"
+                  onClick={handleAddToCart}
+                >
+                  Add To Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Products;
