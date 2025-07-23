9a8b86b5f7dd35d2d81404f65905f2387c528318
slaimohamed <mohmedadel993@gmail.com>
2025-07-24 01:51:54 +0300

    k

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/signup.css";
@@ -6,31 +8,30 @@ import "./styles/details.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import Error404 from "./pages/Error404";  
+import Error404 from "./pages/Error404";
 import Layout from "./pages/layout";
+import Blog from "./pages/blog";
 import { menuData } from "./data";
 import Products from "./pages/products";
 import { CartProvider } from "./Utilits/CartContext";
-import ProductsCart from "./componants/productsCart";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Blog from "./pages/blog"; 
+//import ProductsCart from "./componants/productsCart";
 
 console.log("Menu Data:", menuData);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="Products" element={<Products />} />
-        <Route path="Cart" element={<Cart />} />
-        <Route path="*" element={<Error404 />} />
+  <CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Products" element={<Products />} />
+          <Route path="Cart" element={<Cart />} />
+          <Route path="*" element={<Error404 />} />
 <Route path="blog" element={<Blog />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </CartProvider>
 );
+
