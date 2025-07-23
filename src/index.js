ef2a44cb991da98fa70119867efed334ef3b01be
slaimohamed <mohmedadel993@gmail.com>
2025-07-23 20:31:06 +0300

    blog detail

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/signup.css";
 import "./styles/details.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
+//import Cart from "./pages/Cart";
+import Blog from "./pages/blog"; 
 import Error404 from "./pages/Error404";
 import Layout from "./pages/layout";
-
 import { menuData } from "./data";
 import Products from "./pages/products";
-// import ProductsCart from "./componants/productsCart";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// <Route path="cart" element={<Cart />} />
 
 console.log("Menu Data:", menuData);
 
@@ -23,8 +23,9 @@ root.render(
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="Products" element={<Products />} />
-        <Route path="Cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+       
+        <Route path="blog" element={<Blog />} /> 
         <Route path="*" element={<Error404 />} />
       </Route>
     </Routes>
---
+++ src/pages/blog.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../styles/details.css";
+
+const BlogDetails = () => {
+  return (
+    <div className="container-sm">
+      <div className="main-content">
+        <img src="../assets/salad.jpg" className="blog-image" alt="Salad" />
+        <div className="meta">By Admin | 07 Comments | Date: 09.07.2024</div>
+        <h2>Health Benefits of a Raw food</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint delectus
+          odio nobis non voluptatem laudantium magni voluptas dicta explicabo
+          commodi!
+        </p>
+
+        {/* Related Posts */}
+        <div className="related-posts">
+          <h3>Related Posts</h3>
+          <div className="row gx-3">
+            <div className="col-12 col-lg-6 pb-3">
+              <img src="../assets/juice.jpg" className="img-fluid w-100" alt="Juice" />
+            </div>
+            <div className="col-12 col-lg-6">
+              <img src="../assets/pasta.jpg" className="img-fluid w-100" alt="Pasta" />
+            </div>
+          </div>
+        </div>
+
+        <div className="comment-section">
+          <h3>Comment</h3>
+          <p>
+            <strong>John Martin:</strong> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Asperiores officiis magni explicabo fuga molestiae!
+          </p>
+        </div>
+
+        <div className="tags">
+          <strong>Tags:</strong> Catalog, Appetizer, Meat Food
+        </div>
+
+        <div className="pagination">
+          <button>1</button>
+          <button>2</button>
+          <button>Next</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogDetails;
