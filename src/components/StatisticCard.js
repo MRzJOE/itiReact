50b56f1fbe495ad68dcf23f0dd76e4bbff3dddbe
Yara Samy Fouad Hafez <ysfh_ben_18@edu.helwan.edu.eg>
2025-07-25 13:31:34 +0300

    a

--- src/components/StatisticCard.js
+++ src/components/StatisticCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './StatisticCard.module.css';
+import React from "react";
+import styles from "./StatisticCard.module.css";
 
 const StatisticCard = ({ value, label }) => {
   return (
